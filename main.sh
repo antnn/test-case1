041a557da4f7a8fe38769c25642f74274505967d
Anton Valishin <anton@valishin.ru>
2024-10-18 05:52:26 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,16 +3,21 @@ set -Eeuo pipefail
 set -o nounset
 set -o errexit
 export USER=user
-#Converts it to one line to pass to cmd in SynchronousCommand
+
+
+# Converts it to one line to pass to cmd in SynchronousCommand
+# Runs from cmd, not from powershell
+# Install pwsh 7 to avoid possible powershell 5 bugs in advance
+# see https://github.com/antnn/is-it-pwsh-bug-qm/ and https://github.com/microsoft/CSS-Exchange/issues/1917
 powershell_script=$(cat <<'EOF' | tr '\n' ';' | sed 's/"/\\"/g'
 $defaultEntryPoint = 'start.ps1'
 foreach ($drive in [char]'A'..[char]'Z') {
-    $defaultFirstLogonCmd = 'powershell -NoExit -ExecutionPolicy Bypass -File '
+    $defaultFirstLogonCmd = ' '
     $drive = [char]$drive
     $path = "${drive}:\$defaultEntryPoint" 
-    if (Test-Path $path) { 
-        $defaultFirstLogonCmd = "${defaultFirstLogonCmd} ${path} " 
-        & cmd /C $defaultFirstLogonCmd 
+    if (Test-Path ${path}) {
+      Start-Process msiexec.exe -ArgumentList "/i ${drive}:\pwsh.msi /passive /qb" -Wait
+      Start-Process ${env:ProgramFiles}\PowerShell\7\pwsh.exe -ArgumentList "-NoExit -ExecutionPolicy Bypass -File ${path}"
     } 
 }
 EOF
@@ -20,25 +25,20 @@ EOF
 powershell_script="C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command \"$powershell_script\""
 
 
-BRIDGE=virbr1
 build() {
-    local ISO_TEMP_ROOT_BUILD_DIR="/tmp/iso"
-    local OS_ISO="/iso/win_server.iso"
-    local CONFIG_ISO="/opt/vm/win/config.iso"
-    local WIN_SOCKET_MON="/tmp/qemu.win.socket"
+  local TEMPLATE_DIR="$PROJECT_DIR/templates"
+  local ISO_TEMP_ROOT_BUILD_DIR="/tmp/iso"
+  local CONFIG_ISO="$WIN_VM_DIR/config.iso"
+  local WIN_SOCKET_MON="/tmp/qemu.win.socket"
+  local WIN_MAC_ADDRESS="00:11:22:33:44:55"
+  local WIN_IP_ADDRESS="192.168.88.2/24"
+  local ROUTER_IP="192.168.88.1"
+  mkdir -p "$ISO_TEMP_ROOT_BUILD_DIR/iso"
+  mkdir -p "$(dirname "$CONFIG_ISO")"
 
-    local WIN_MAC_ADDRESS="00:11:22:33:44:55"
-    local WIN_IP_ADDRESS="192.168.152.3/24"
-
-    local ROUTER_IP="192.168.152.2"
-
-    /opt/project/render_template.sh /opt/project/internal.net.template.xml bridge="$BRIDGE" > /opt/project/internal.net.xml
-    
-    mkdir -p "$ISO_TEMP_ROOT_BUILD_DIR/iso"
-    mkdir -p "$(dirname "$CONFIG_ISO")"
-
-    /opt/project/generate_autounattend.sh --image-index "2" \
-        --template-file "/opt/project/autounattend.template.xml" \
+    echo Generating answerfile
+    "$PROJECT_DIR"/generate_autounattend.sh --image-index "2" \
+        --template-file "$TEMPLATE_DIR/autounattend.template.xml" \
         --output-dir "$ISO_TEMP_ROOT_BUILD_DIR/iso" \
         --lang "ru-RU" --user-name "IEUSER" --user-password "Passw0rd!" \
         --admin-password "Passw0rd!" --computer-name "MAINSERVER" \
@@ -46,43 +46,74 @@ build() {
         --route-prefix "0.0.0.0/0" --default-gw "$ROUTER_IP" --dns-server "127.0.0.1" \
         --secondary-dns "1.1.1.1" --setup-command "$powershell_script"
 
-    /opt/project/create_config_iso.sh --build-dir "$ISO_TEMP_ROOT_BUILD_DIR" \
-        --output-path "$CONFIG_ISO" \
-        --virtio-url "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-2/virtio-win-0.1.262.iso" \
-        --virtio-checksum "bdc2ad1727a08b6d8a59d40e112d930f53a2b354bdef85903abaad896214f0a3"
-    
-    local WIN_DRIVE="/opt/vm/win/win.qcow2"
-    echo Creating disk for Windows Server...
-    qemu-img create -f qcow2  "$WIN_DRIVE" 120G
-    chown user $WIN_DRIVE
 
-    echo Rendering Windows Server domain template...
-    /opt/project/render_template.sh /opt/project/win2k22.template.xml \
+    "$PROJECT_DIR"/create_config_iso.sh --build-dir "$ISO_TEMP_ROOT_BUILD_DIR" \
+        --output-path "$CONFIG_ISO"
+
+    local WIN_DRIVE="$WIN_VM_DIR/win.qcow2"
+    echo "Creating disk for Windows Server..."
+    qemu-img create -f qcow2  "$WIN_DRIVE" "$WIN_DRIVE_SIZE"
+    chown $USER "$WIN_DRIVE"
+
+    echo Rendering Windows Server KVM domain template...
+    "$PROJECT_DIR"/render_template.sh "$TEMPLATE_DIR"/win2k22.template.xml \
+        name="$WIN_VM_NAME" \
         drive="$WIN_DRIVE" \
         mac_address="$WIN_MAC_ADDRESS" \
         bridge="$BRIDGE" \
-        os_iso="$OS_ISO" \
+        os_iso="$WIN_ISO" \
         socket_mon="$WIN_SOCKET_MON" \
-        config_iso="$CONFIG_ISO" > /opt/vm/win/win2k22.domain.xml
-    
-    curl 
+        config_iso="$CONFIG_ISO" > "$WIN_XML_FILE"
+
+    echo "Defining internal network for windows server and routeros..."
+    "$PROJECT_DIR"/render_template.sh "$TEMPLATE_DIR"/internal.net.template.xml name="$NET_NAME" bridge="$BRIDGE" > "$VM_DIR/net.xml"
+    echo "allow $BRIDGE" >> /etc/qemu/bridge.conf
+
+    mkdir -p  "$ROS_VM_DIR"
+    mv "$DOWNLOADS/$ROS_DRIVE" "$ROS_VM_DISK"
+    echo "Generating domain xml for RouterOS"
+    "$PROJECT_DIR"/render_template.sh "$TEMPLATE_DIR"/routeros.template.xml \
+      name="$ROS_VM_NAME"\
+      drive="$ROS_DRIVE"\
+      interface1="virbr0" \
+      interface2="$BRIDGE" > "$ROS_XML_FILE"
 }
 
 # Function to run commands as user
 #BRIDGE=virbr1
 run_as_root() {
-    echo "allow $BRIDGE" >> /etc/qemu/bridge.conf
-    systemctl enable --now virtnetworkd-ro.socket
-    virsh net-define /opt/project/internal.net.xml
-    virsh net-start private && virsh net-autostart private
-    chown -R user /tmp/wayland-0
-    # Fixing wayland
-    bash -c "sleep 3; mkdir -p /run/user/1000; ln -sf /tmp/wayland-0 /run/user/1000/wayland-0 ; chown -R user /run/user/1000" &
-    sudo -u user bash
+  UNIT_FILE="/etc/systemd/system/firstlogon.service"
+  cat << EOF > "$UNIT_FILE"
+[Unit]
+Description=Custom VM Setup Service
+After=network.target libvirtd.service
+Requires=libvirtd.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/virsh net-define "$VM_DIR/net.xml"
+ExecStart=/bin/bash -c '/usr/bin/virsh net-start $NET_NAME && /usr/bin/virsh net-autostart $NET_NAME'
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Set proper permissions for the unit file
+  chmod 644 "$UNIT_FILE"
+  sudo systemctl enable --now firstlogon.service
 }
 run_as_user() {
+    echo Defining RouterOS domain...
+    virsh define $ROS_XML_FILE
     echo Defining Windows Server KVM domain...
-    virsh define /opt/vm/win/win2k22.domain.xml
+    virsh define "$WIN_XML_FILE"
+
+    echo Starting console script
+    python3 /opt/project/console.py &
+    virsh start "$WIN_VM_NAME"
+    virsh start "$ROS_VM_NAME"
+
     virt-manager -c qemu:///session
 }
 
